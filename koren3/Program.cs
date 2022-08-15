ffdb9db4f791181a0fa1903cc4345747b4199aad
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-08-15 18:16:25 +0530

    добаваили

---
+++ koren3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] x)
+{
+  int count = x.Length;
+
+  for (int index = 1; index < count; index++)
+
+    Console.Write(x[index]+ " ");
+    
+  }
+ 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
+
