8d22938bedbd3c33e53828463642d6b6a7db95fa
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-08-15 18:06:46 +0530

    добаваили

---
+++ koren/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число из пяти цифр: ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+ else Console.WriteLine($"Ваше число: {number} - Не палиндром.");
+ }
+
+ if (number!.Length == 5){
+  CheckingNumber(number);
+ }
+ else Console.WriteLine($" введите число из пяти цифр");
